965912f83b11b37d80e310db849d13272a58b568
Sori Kang <56942589+Kaaaaaaaaaang@users.noreply.github.com>
2020-06-26 02:43:54 +0900

    Add files via upload

--- C_Student/Student/Student/Main.cpp
+++ C_Student/Student/Student/Main.cpp
@@ -2,12 +2,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// C언어_성적 처리 프로그램
-// 성적 처리할 학생들의 정보들이 있는 텍스트 파일을 불러와 분석한다.
-// 100명 이하의 학생들을 처리
-// 학생들의 학번, 이름, 국어, 영어, 수학 성적을 입력받는다.
-// 각 학생의 총점과 평균, 각 과목의 평균을 출력한다.
-// 출력은 총점이 높은 순(등수 순)으로 한다.
 
 typedef struct STUDENT
 {
@@ -55,17 +49,18 @@ int main(void)
 		}
 	}
 	printf("\n                             성적처리                        \n\n");
-	printf("=====================================================================\n");
-	printf("   학번      이름      국어      영어      수학      총점      평균\n");
-	printf("=====================================================================\n");
+	printf("=======================================================================\n");
+	printf("   학번      이름      국어      영어      수학      총점       평균 \n");
+	printf("=======================================================================\n");
 	for (int k = 0; k < i; k++) {
-		//%7d 폭 = 7, 우측으로 출력 %7.2f 소수점 두자리에서 반올림
-		printf("  %s  %12s  %7d  %7d  %7d  %7d  %7.2f\n", people[k].num, people[k].name, people[k].kor, people[k].eng, people[k].math, people[k].sum, people[k].avg);
+		//%7d 폭 = 7, 우측으로 출력 %7.2f 소수점 첫자리로 반올림
+		printf("  %s  %12s  %8d  %8d  %8d  %8d  %8.1f\n",
+			people[k].num, people[k].name, people[k].kor, people[k].eng, people[k].math, people[k].sum, people[k].avg);
 	}
-	printf("=====================================================================\n");
-	printf("  %s  %12s  %7d  %7d  %7d  %7d  %7.2f\n", total.num, total.name, total.kor, total.eng, total.math, total.sum, total.avg);
-	printf("  %s  %12s  %7.2f  %7.2f  %7.2f  %7.2f  %7.2f\n", total.num, total.name, total.kor / (double)i, total.eng / (double)i, total.math / (double)i, total.sum / (double)i, total.avg / (double)i);
-	printf("=====================================================================\n");
+	printf("=======================================================================\n");
+	printf("  과목당 평균 점수 %5.1f점  %5.1f점  %5.1f점   전체 평균 점수 %5.1f점\n",
+		total.kor / (double)i, total.eng / (double)i, total.math / (double)i, total.avg / (double)i);
+	printf("=======================================================================\n");
 	return 0;
 }
 
